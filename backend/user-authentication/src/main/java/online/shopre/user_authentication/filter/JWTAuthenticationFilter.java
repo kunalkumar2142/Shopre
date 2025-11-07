3790f1b14dd66c7e2bdb3bb4f75dfd2d46fdbf1d
Kunal Kumar <4002kunal88@gmail.com>
2025-11-07 22:36:58 +0530

    JWTauthentication filer is on progress

--- backend/user-authentication/src/main/java/online/shopre/user_authentication/filter/JWTAuthenticationFilter.java
+++ backend/user-authentication/src/main/java/online/shopre/user_authentication/filter/JWTAuthenticationFilter.java
@@ -1,25 +1,24 @@
-//package online.shopre.user_authentication.filter;
-//
-//import jakarta.servlet.FilterChain;
-//import jakarta.servlet.ServletException;
-//import jakarta.servlet.http.HttpServletRequest;
-//import jakarta.servlet.http.HttpServletResponse;
-//import org.apache.tomcat.util.http.fileupload.IOUtils;
-//import org.springframework.stereotype.Component;
-//import org.springframework.web.filter.OncePerRequestFilter;
-//
-//import java.io.IOException;
-//
-//@Component
-//public class JWTAuthenticationFilter extends OncePerRequestFilter {
-//
-//    @Override
-//    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
-//            throws ServletException, IOException {
-//        String authorizationHeader = request.getHeader("Authorization");
-//        String jwtToken;
-//        String userEmail;
-//
-//        if()
-//    }
-//}
+package online.shopre.user_authentication.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JWTAuthenticationFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(@NotNull HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        String authorizationHeader = request.getHeader("Authorization");
+        String jwtToken;
+        String userEmail;
+    }
+}
