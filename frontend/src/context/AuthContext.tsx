7e223cf1f0774d3fc21847191ff3a45ea54e7abd
Kunal Kumar <4002kunal88@gmail.com>
2026-02-14 00:53:53 +0530

    -14feb 2026

---
+++ frontend/src/context/AuthContext.tsx
@@ -0,0 +1,14 @@
+import type { SignInRequest } from "@/types/auth";
+import type { Usersession } from "@/types/user-session";
+import { createContext } from "react";
+
+interface AuthContextType{
+    UserSession: Usersession | null;
+    isAuthenticated: boolean;
+    isAdmin: boolean;
+
+    login: (SignInRequest: SignInRequest) => Promise<boolean>
+    logout: () => void;
+}
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
