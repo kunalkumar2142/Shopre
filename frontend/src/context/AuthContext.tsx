5b7fadb28d8b23c337547e90ed77a5a967f1bf6d
Kunal Kumar <4002kunal88@gmail.com>
2026-02-28 16:05:42 +0530

    AuthContext.tsx is completed -28feb 2026

--- frontend/src/context/AuthContext.tsx
+++ frontend/src/context/AuthContext.tsx
@@ -1,8 +1,9 @@
 import { signIn } from "@/api/auth";
 import type { SignInRequest } from "@/types/auth";
 import type { Usersession } from "@/types/user-session";
-import { LogIn } from "lucide-react";
-import { createContext, useState, type ReactNode } from "react";
+import { error } from "console";
+import { LogIn, LogOut } from "lucide-react";
+import { createContext, useContext, useState, type ReactNode } from "react";
 
 interface AuthContextType{
     UserSession: Usersession | null;
@@ -13,6 +14,14 @@ interface AuthContextType{
     logout: () => void;
 }
 
+const getRoleFromToken = (token: string) => {
+    try{
+        const payload = JSON.parse(atob(token.split(".")[1]))
+        return payload.role || "user"
+    }catch(error){
+        return "user"
+    }
+}
 
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
@@ -21,14 +30,40 @@ export const AuthProvider = ({ children } : { children : ReactNode }) => {
        const storedUser = localStorage.getItem("userSession");
        return storedUser ? JSON.parse(storedUser) : null; 
     });
+    
     const login = async (SignInRequest : SignInRequest): Promise<boolean> => {
-        const response = await signIn(SignInRequest);
-        const {email,token,name} = response.data;
-        localStorage.setItem("userSession", JSON.stringify({email,token,name}));
-        return true;
+        try{
+            const response = await signIn(SignInRequest);
+            const {email,token,name} = response.data;
+            localStorage.setItem(
+                "userSession", 
+                JSON.stringify({email,token,name})
+            );
+            return true;
+        }catch(error){
+            throw(error);
+        }
     };
 
     const logout = async () => {
 
     };
+
+    const value = = {
+        userSession,
+        isAuthenticated: !!userSession,
+        isAdmin: userSession ? getRoleFromToken(userSession.token) === "ADMIN",
+        login,
+        logout
+    }
+
+    return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>
+}
+
+export const UseAuth = (): AuthContextType => {
+    const context = useContext(AuthContext);
+    if(context === undefined){
+        throw new error("useauth must ued in AuthProvider")
+    }
+    return context;
 }
