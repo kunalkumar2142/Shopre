e7805fc83380f7b1792e594d9c1c6c1bf2ab9318
Kunal Kumar <4002kunal88@gmail.com>
2026-03-02 23:53:31 +0530

    re initialise the frontend -2mar 2026

--- frontend/src/pages/MyComponent.tsx
+++
@@ -1,5 +0,0 @@
-export default function MyComponent(){
-    return(
-        <div>MyComponent</div>
-    )
-}
--- frontend/src/types/user-session.ts
+++
@@ -1,5 +0,0 @@
-export interface Usersession{
-    email: string;
-    name: string;
-    token: string;
-}
