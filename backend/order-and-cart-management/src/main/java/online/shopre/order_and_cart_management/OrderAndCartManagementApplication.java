f2bc5e8c863ce7881a874629f5bfa0d6d92337f8
Kunal Kumar <4002kunal88@gmail.com>
2026-05-27 23:03:41 +0530

    order-and-cart-managment service initalised succesfully  -27may 2026

---
+++ backend/order-and-cart-management/src/main/java/online/shopre/order_and_cart_management/OrderAndCartManagementApplication.java
@@ -0,0 +1,13 @@
+package online.shopre.order_and_cart_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderAndCartManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderAndCartManagementApplication.class, args);
+	}
+
+}
---
+++ backend/order-and-cart-management/src/test/java/online/shopre/order_and_cart_management/OrderAndCartManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package online.shopre.order_and_cart_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderAndCartManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
