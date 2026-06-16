ea12c8657d9bff9356a2f87ef35ccb3aded84953
Kunal Kumar <4002kunal88@gmail.com>
2026-06-16 22:28:49 +0530

    X-USER-ID is added to the header -14june 2026

--- backend/order-and-cart-management/src/main/java/online/shopre/order_and_cart_management/repository/CartItemRepository.java
+++ backend/order-and-cart-management/src/main/java/online/shopre/order_and_cart_management/repository/CartItemRepository.java
@@ -10,6 +10,5 @@ import java.util.UUID;
 
 @Repository
 public interface CartItemRepository  extends JpaRepository<CartItem, UUID> {
-
     Optional<CartItem> findByCartAndProductId(Cart cart, UUID productId);
 }
