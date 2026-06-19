edf3427952eefe49226f8a45c7e3174cf4ae2c40
Kunal Kumar <4002kunal88@gmail.com>
2026-06-20 02:11:40 +0530

    userId added to the JWT Token  -20june 2026

--- backend/user-authentication/src/main/java/online/shopre/user_authentication/service/JwtService.java
+++ backend/user-authentication/src/main/java/online/shopre/user_authentication/service/JwtService.java
@@ -40,7 +40,7 @@ public class JwtService {
         Map<String, Object> claims = new HashMap<>();
         if (userDetails instanceof User user) {
             claims.put("role", user.getRole().name());
-
+            claims.put("userId", user.getId());
         }
         return Jwts
                 .builder()
